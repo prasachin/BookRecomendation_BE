35c7840836b35b951232991d9bbf4d726bf441c7
sachin <sachinp@iitbhilai.ac.in>
2024-07-16 12:31:34 +0530

    rate and review

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Book = require("../models/book");
 const Review = require("../models/review");
 
 router.post("/review", async (req, res) => {
