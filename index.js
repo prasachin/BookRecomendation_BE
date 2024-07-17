89b8f1dedf49b0e37ecd7432ea45fb2bb0f71207
sachin <sachinp@iitbhilai.ac.in>
2024-07-17 21:22:20 +0530

    UI update

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const io = socketIo(server);
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' 'unsafe-inline';"
+  );
+  next();
+});
+
 io.on("connection", (socket) => {
   console.log("New client connected");
 
